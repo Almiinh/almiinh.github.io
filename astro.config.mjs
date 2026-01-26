5d193632a4d26850de642586408ed4ceb4b3c821
Minh-Hoang HUYNH <minh-hoang.huynh@outlook.fr>
2026-01-26 08:24:44 +0100

    Blog: Remove Tailwind + Remove template posts

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,22 +7,16 @@ import remarkMath from "remark-math";
 import remarkGfm from "remark-gfm";
 import remarkCallout from "@r4ai/remark-callout";
 
-import tailwindcss from "@tailwindcss/vite";
-
 // https://astro.build/config
 export default defineConfig({
-  site: "https://almiinh.github.io",
-  integrations: [svelte(), mdx()],
+    site: "https://almiinh.github.io",
+    integrations: [svelte(), mdx()],
 
-  redirects: {
-      "/web": "/#web-projects",
-  },
+    redirects: {
+        "/web": "/#web-projects",
+    },
 
-  markdown: {
-      remarkPlugins: [remarkExtendedTable, remarkMath, remarkGfm, remarkCallout],
-  },
-
-  vite: {
-    plugins: [tailwindcss()],
-  },
+    markdown: {
+        remarkPlugins: [remarkExtendedTable, remarkMath, remarkGfm, remarkCallout],
+    },
 });
